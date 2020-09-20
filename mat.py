8c7718efa0d285b10965afa619195ddb7973d7a9
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-21 00:32:09 +0200

    combined conversion matrices

--- mat.py
+++ mat.py
@@ -25,20 +25,27 @@ blue_LMS = M_xyz_to_lms @ blue_XYZ
 white_LMS = M_xyz_to_lms @ white_XYZ
 
 a = np.linspace(0, 1, 100)
+zeros = np.zeros_like(a)
+ones = np.ones_like(a)
 
 # L-weak
 q2l = (1 - a) * (blue_LMS[1] - blue_LMS[0]) / (blue_LMS[1] - blue_LMS[2])
 q1l = 1 - a - q2l
+SL = np.stack([a, q1l, q2l, zeros, ones, zeros, zeros, zeros, ones], axis=1).reshape(-1, 3, 3)
 
 
 # M-weak
 q2m = (1 - a) * (blue_LMS[0] - blue_LMS[1]) / (blue_LMS[0] - blue_LMS[2])
 q1m = 1 - a - q2m
+SM = np.stack([ones, zeros, zeros, q1m, a, q2m, zeros, zeros, ones], axis=1).reshape(-1, 3, 3)
 
 # S-weak
 q2s = (1 - a) * (red_LMS[0] - red_LMS[2]) / (red_LMS[0] - red_LMS[1])
 q1s = 1 - a - q2s
+SS = np.stack([ones, zeros, zeros, zeros, ones, zeros, q1s, q2s, a], axis=1).reshape(-1, 3, 3)
 
+# LMS-weak combined
+SLMS = np.stack([SL, SM, SS]) # Axes: deficiency (lms), a, i, j
 
 # Cone monochromats
 S_mono_cone = np.array([[0, 0, 1],
