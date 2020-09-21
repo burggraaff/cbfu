f4b4a7ba9c6ecae10b464ed5bdcd8d9a5ee8fec6
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-21 18:26:28 +0200

    Create FU plot

--- convert.py
+++ convert.py
@@ -2,18 +2,35 @@ import numpy as np
 import mat
 import fu
 from spectacle.linearity import sRGB_generic
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, patches
 
 FU_LMS_deficiency = np.einsum("caij,fj->cafi",mat.SLMS, fu.FU) # axes: deficiency (lms), a, FU number, lms
 FU_deficient_XYZ = np.einsum("ij,cafj->cafi", mat.M_lms_to_xyz, FU_LMS_deficiency) # axes: deficiency (lms), a, FU number, xyz
 FU_deficient_RGB = np.einsum("ij,cafj->cafi", mat.M_xyz_to_rgb, FU_deficient_XYZ) # axes: deficiency (lms), a, FU number, rgb (linear)
 
-FU_deficient_sRGB = sRGB_generic(FU_deficient_RGB, normalization=1).astype(np.uint8) # Gamma-expanded (non-linear) sRGB values. Note these are clipped to 0-255 to accommodate the limited gamut of sRGB.
+FU_deficient_sRGB = sRGB_generic(FU_deficient_RGB, normalization=1)/255. # Gamma-expanded (non-linear) sRGB values. Note these are clipped to 0-255 to accommodate the limited gamut of sRGB.
 
 example_indices = ((0, 0, 0, 1, 1, 2, 2), (-1, 50, 0, 50, 0, 50, 0))
 examples_sRGB = FU_deficient_sRGB[example_indices]
+examples_labels = ["Regular", "50% L-deficient", "Fully L-deficient", "50% M-deficient", "Fully M-deficient", "50% S-deficient", "Fully S-deficient"]
 
-fig, ax = plt.subplot(111, figsize=(10, 10))
+kwargs = {"width": 0.9, "height": 0.9, "edgecolor": "none"}
+fig, ax = plt.subplots(figsize=(9, 3))
+for i, (FU_list, label) in enumerate(zip(examples_sRGB[::-1], examples_labels[::-1])):
+    print(i, label)
+    rectangles = [patches.Rectangle(xy=(j,i), facecolor=rgb, **kwargs) for j, rgb in enumerate(FU_list)]
+    for rect in rectangles:
+        ax.add_patch(rect)
 
+ax.axis("equal")
+ax.set_yticks(np.arange(6.5, 0, -1))
+ax.set_yticklabels(examples_labels)
+
+ax.set_xticks(np.arange(0.5, 21, 1))
+ax.set_xticklabels(np.arange(1, 22))
+ax.set_xlabel("Forel-Ule colour")
+ax.tick_params(axis="x", bottom=False, labelbottom=False, labeltop=True)
+
+plt.savefig("FU_example.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
