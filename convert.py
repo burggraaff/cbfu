d431b3552b965a4475d3b12f32ea8a6917c295a4
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-21 17:56:54 +0200

    select example colours

--- convert.py
+++ convert.py
@@ -8,3 +8,6 @@ FU_deficient_XYZ = np.einsum("ij,cafj->cafi", mat.M_lms_to_xyz, FU_LMS_deficienc
 FU_deficient_RGB = np.einsum("ij,cafj->cafi", mat.M_xyz_to_rgb, FU_deficient_XYZ) # axes: deficiency (lms), a, FU number, rgb (linear)
 
 FU_deficient_sRGB = sRGB_generic(FU_deficient_RGB, normalization=1).astype(np.uint8) # Gamma-expanded (non-linear) sRGB values. Note these are clipped to 0-255 to accommodate the limited gamut of sRGB.
+
+example_indices = ((0, 0, 0, 1, 1, 2, 2), (0, 50, -1, 50, -1, 50, -1))
+examples_sRGB = FU_deficient_sRGB[example_indices]
