e0a144d69383dfe0362e2710bb07cdaa77cabdcf
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-11-02 14:56:55 +0100

    Reverse colour bars

--- analysis.py
+++ analysis.py
@@ -118,7 +118,7 @@ extreme_labels = ["Regular", "L-def.", "M-def.", "S-def."]
 def plot_distance_matrices(FU_distance_matrices, saveto="image.pdf", title="", ylabel="Euclidean distance (XYZ)", nr_samples=None, **kwargs):
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(col2,5), gridspec_kw={"wspace": 0.3, "hspace": 0.3})
     for ax, distances, label in zip(axs.ravel(), FU_distance_matrices[extreme_indices], extreme_labels):
-        im = ax.imshow(distances, extent=(0, 21, 21, 0), cmap=cm.get_cmap("cividis", nr_samples), **kwargs)
+        im = ax.imshow(distances, extent=(0, 21, 21, 0), cmap=cm.get_cmap("cividis_r", nr_samples), **kwargs)
         ax.set_title(f"\n{label}")
         ax.set_xlim(0, 21)
         ax.set_ylim(0, 21)
